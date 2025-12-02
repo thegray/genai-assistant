229e170834d578b3664026e467d76d65eb14ce1d
paulus.bangun <pbangunk@gmail.com>
2025-12-02 20:10:14 +0700

    update backend url in config

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
     // Change this to your API Gateway URL in production
     // Example: https://xxxxxx.execute-api.ap-southeast-1.amazonaws.com/chat
-  apiUrl: "http://localhost:8000/chat", // local default
+  apiUrl: "https://pov3qxpa7a.execute-api.ap-southeast-1.amazonaws.com/chat",
 };
